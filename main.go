563a3f14e8df8635d40cea1340734861544c58cc
esJohan <imjohanjjcc@gmail.com>
2023-02-28 23:46:59 -0600

    firstGo

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import ( 
+	"fmt" 
+	"github.com/google/uuid" )
+
+func main() {
+	fmt.Println("Hello World By Go")
+	fmt.Println("This is a the second line")
+	fmt.Println("This is the third line edited by nano")
+	fmt.Println(uuid.New().String());
+}
+
+
+func inn(){
+	int x;9
+	int a =0
+	int 
+}
